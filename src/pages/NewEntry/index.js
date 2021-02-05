2cf1a01e326e0d1c1c58426f3e8ea5ce790f2323
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-02-04 21:15:09 -0300

    New Entry index.js start

--- src/pages/NewEntry/index.js
+++ src/pages/NewEntry/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 
 import BalanceLabel from "./components/BalaceLabel"
 
 const NewEntry = () => {
     return (
-        <View>
+        <View style={styles.container}>
             <BalanceLabel></BalanceLabel>
         </View>
     )
